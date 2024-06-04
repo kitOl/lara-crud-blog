18f2445d2310231eb6b313789b83bd6c23a9a53f
Oleg Kitaev <kitaev_oleg@mail.ru>
2024-06-04 12:02:01 +0300

    add basic files and create model and controller

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $posts = Post::all();
+        return view('posts.index', compact('posts'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('posts.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $post = Post::create([
+            'title' => $request->title,
+            'description' => $request->description
+        ]);
+
+        if ($post) {
+            return redirect()->route('posts.index');
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Post $post)
+    {
+        return view('posts.show', compact('post'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Post $post)
+    {
+        return view('post.edit', compact('post'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Post $post)
+    {
+        $post['title'] = $request->title;
+        $post['description'] = $request->description;
+        $post->save();
+        return redirect()->route('posts.index');
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(post $post)
+    {
+        $post->delete();
+        return redirect()->route('posts.index');
+    }
+}
